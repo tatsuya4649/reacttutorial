e0b070595900aca8eeabb6946ca4b17e093afa8c
tatsuya <tatsuya4649developer@gmail.com>
2021-07-06 20:27:05 +0900

    5 add

--- 5/src/js/pages/Layout.js
+++ 5/src/js/pages/Layout.js
@@ -1,17 +1,26 @@
 
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink,Link,withRouter } from "react-router-dom";
 
-export default class Layout extends React.Component{
+class Layout extends React.Component{
+	navigate(){
+		console.log(this.props.history);
+		this.props.history.push("/");
+	}
 	render(){
 		console.log(this.props.children);
 		return (
 		<div>
 		<h1>KillerNews.net</h1>
 		{this.props.children}
-		<Link to="/archives"><button class="btn btn-danger">archives</button></Link>
-		<Link to="/settings"><button class="btn btn-success">settings</button></Link>
+		<Link to="/archives/some-other-articles?date=yesterday&filter=none" class="btn btn-warning">archives (some other articles)</Link>
+		<Link to="/archives?date=today&filter=hot" class="btn btn-danger">archives</Link>
+		<NavLink to="/settings/main" class="btn btn-success" activeClassName="btn-danger">settings</NavLink>
+		<Link to="/settings/extra"><button class="btn btn-success">settings (extra)</button></Link>
+		<button class="btn btn-info" onClick={this.navigate.bind(this)}>featured</button>
 		</div>
 		);
 	}
 }
+
+export default withRouter(Layout);
