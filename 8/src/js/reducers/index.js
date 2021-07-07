133f887a7b3c74754f64f15fd7db9d438d3ca648
tatsuya <tatsuya4649developer@gmail.com>
2021-07-07 17:22:33 +0900

    redux/react

---
+++ 8/src/js/reducers/index.js
@@ -0,0 +1,11 @@
+
+
+import { combineReducers } from "redux";
+
+import tweetsReducer from "./tweetsReducer";
+import userReducer from "./userReducer";
+
+export default combineReducers({
+	tweetsReducer,
+	userReducer
+});
---
+++ 8/webpack.config.js
@@ -0,0 +1,38 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+	context: path.join(__dirname,"src"),
+	entry: "./js/client.js",
+	module: {
+		rules: [{
+			test: /\.jsx?$/,
+			exclude: /(node_modules|bower_components)/,
+			use: [{
+				loader: 'babel-loader',
+				options: {
+					plugins: [
+					  'react-html-attrs',
+					  [require('@babel/plugin-proposal-decorators'),{legacy: true}],
+					],
+					presets: ['@babel/preset-react','@babel/preset-env']
+				}
+			}]
+		}]
+	},
+	output: {
+		path: __dirname + "/src",
+		filename: "client.min.js",
+		publicPath: '/'
+	},
+	devServer: {
+		contentBase: __dirname + '/src',
+		historyApiFallback: true
+	},
+	plugins: debug ? [] : [
+		new webpack.optimize.OccurrenceOrderPlugin(),
+		new webpack.optimize.UglifyJsPlugin({mangle:false,sourcemap: false})
+	]
+};
+
